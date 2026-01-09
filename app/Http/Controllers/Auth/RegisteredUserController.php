d7b2c512a9cd72ba2468079887b6fe12557e2638
madrizkymk <madrizkymk@gmail.com>
2026-01-09 15:24:28 +0700

    n

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'phone' => ['required', 'string', 'regex:/^(\+62|62|0)[8-9][0-9]{7,11}$/', 'max:15'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone_number' => $request->phone,
             'password' => Hash::make($request->password),
         ]);
 
